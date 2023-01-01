e23a5139aa9c3475ab638288bd747a95a2037336
jorgedaktika <jorge@daktika.com>
2023-01-01 23:05:02 +0100

    change

--- test/addTest.php
+++ test/addTest.php
@@ -3,6 +3,12 @@ namespace App\Test;
 
 use App\MyClass;
 
+class MyClass {
+    public function add($a, $b){
+        return $a + $b;
+    }
+}
+
 class Test1Test extends \PHPUnit\Framework\TestCase {
 
 	public function testAdd(): void  {
