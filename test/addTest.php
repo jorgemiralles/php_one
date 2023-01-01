d34811d6d699865988ed89101d22b484de2c3f0d
jorgedaktika <jorge@daktika.com>
2023-01-01 23:10:31 +0100

    make fail test

--- test/addTest.php
+++ test/addTest.php
@@ -13,7 +13,7 @@ class Test1Test extends \PHPUnit\Framework\TestCase {
 
 	public function testAdd(): void  {
 		$myObj = new MyClass();
-		$this->assertEquals($myObj->add(1,2), 3);
+		$this->assertEquals($myObj->add(1,2), 4);
 	}
 
 }
