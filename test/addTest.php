0c5ca56a73a6baa9815197f864d66f636bbe15cd
jorgedaktika <jorge@daktika.com>
2023-01-02 23:56:50 +0100

    make pass with 4

--- test/addTest.php
+++ test/addTest.php
@@ -13,7 +13,7 @@ class Test1Test extends \PHPUnit\Framework\TestCase {
 
 	public function testAdd(): void  {
 		$myObj = new MyClass();
-		$this->assertEquals($myObj->add(1,2), 4);
+		$this->assertEquals($myObj->add(1,2), 3);
 		$this->assertEquals($myObj->add(-1,4), 3);
 		$this->assertEquals($myObj->add(0,0), 0);
 	}
